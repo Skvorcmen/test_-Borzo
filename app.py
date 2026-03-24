48d564f45edd29dd3218c0acb8bbd6af47eb1cde
skvorcmen <skvorcmen@gmai.com>
2026-03-24 12:56:20 +0500

    Fix: prevent multiple message alerts for manager

--- app.py
+++ app.py
@@ -603,5 +603,5 @@ def get_messages():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, host="0.0.0.0", port=port)
