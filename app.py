4b07fe22693c5f8026aebc6e1ff7125a13164ec8
skvorcmen <skvorcmen@gmai.com>
2026-03-24 17:38:36 +0500

    Remove duplicate copyAttachment function

--- app.py
+++ app.py
@@ -583,3 +583,5 @@ def get_messages():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, host="0.0.0.0", port=port)
+
+# полностью рабочий код но фото скачивается
