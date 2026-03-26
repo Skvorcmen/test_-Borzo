ca5548b25e43cadd92ad262ad4f0d6c9fe7873f8
skvorcmen <skvorcmen@gmai.com>
2026-03-26 10:54:02 +0500

    Add Procfile for Railway deployment

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ def get_messages():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Измените на 10000
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
 
 # полностью рабочий код но фото скачивается
